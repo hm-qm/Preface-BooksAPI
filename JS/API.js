7563f908602e0366e540039ed582c0786d8ad5e2
hm-qm <77652802+hm-qm@users.noreply.github.com>
2021-06-29 21:19:59 +1000

    added search bar functionality, and rendering items to page

--- JS/API.js
+++ JS/API.js
@@ -1,10 +1,4 @@
-// export const getBreeds = async (page) => {
-//     const responsePromise = fetch(`https://catfact.ninja/breeds?page=${page}`);
-//     const response = await responsePromise;
-//     const jsonResponse = await response.json();
-//     return jsonResponse.data;
-// };
-
+import * as utilities from './Utilities.js'
 
 export const getBooks = async (query, startIndex=0, maxResults=10) => {
     const responsePromise = fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}&startIndex=${startIndex}&maxResults=${maxResults}`);
@@ -12,10 +6,33 @@ export const getBooks = async (query, startIndex=0, maxResults=10) => {
     const dataJson = await response.json()
     const bookItems = dataJson
 
-    console.log(dataJson)
     return bookItems;
 }
 
-getBooks('harry potter')
+
+export const searchResults = async (query, startIndex=0, maxResults=10) => {
+    const rawBookData = await getBooks(query, startIndex, maxResults)
+
+    const cleanBookData = rawBookData.items.map((book) => ({
+        title: book.volumeInfo.title,
+        authors: book.volumeInfo.authors || [""],
+        description: book.volumeInfo.description || "",
+        pages: book.volumeInfo.pageCount || "",
+        link: book.volumeInfo.canonicalVolumeLink,
+        preview: book.volumeInfo.previewLink,
+        publisher: book.volumeInfo.publisher || "",
+        publishedDate: book.volumeInfo.publishedDate || "",
+        categories: book.volumeInfo.categories || [""],
+        image: book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : "https://via.placeholder.com/100x150"
+    }))
+
+    console.log(cleanBookData)
+    return cleanBookData;
+}
 
 
+export const renderSearch = async(query, index=0, maxResults=10) => {
+    const results = await searchResults(query, index, maxResults);
+
+    results.map((book, index) => utilities.constructBookCard(book, index));
+}
---
+++ JS/Utilities.js
@@ -0,0 +1,66 @@
+import * as dom from './DOM.js'
+
+
+
+//Creates any element with parameterized classnames and innerHTML
+export const createElement = (elementType='div', classname, key, innerHTML) => {
+    const newElement = document.createElement(elementType);
+
+    if(key){
+        newElement.dataset.index = key;
+    }
+
+    if (innerHTML){
+        newElement.innerHTML = innerHTML;
+    }
+    
+    newElement.classList.add(classname);
+
+    return newElement;
+}
+
+// Appends an array of elements to any query selectable parent element
+export const appendElements = (parentElement, elementArr) => {
+
+    const elementsToAppend = [...elementArr];
+
+    elementsToAppend.map(element => parentElement.append(element));
+};
+
+
+export const constructBookCard = (data, index) => {
+    
+    //Define the innerHTML content for each div section in card
+    const divImg = `<img src=${data.image}>`;
+    const divTitle = `<h3>${data.title}</h3>`;
+    const divSubTitle = `<span>${data.authors.join(", ")}</span>\u00A0\u00A0\u00A0<span>${data.publishedDate.slice(0,4)}</span>`;
+    const divCategories = data.categories.map(category => `<span>${category}<span>`).join(" ");
+    const divDescription = `<p>${data.description.replace(/^(.{300}[^\s]*).*/, "$1")}...</p>`;
+    const divButtons = `<button>More Info</button><button>Buy</button>`;
+
+    //Build the initial card section
+    const section = [createElement('section', `bookCard`, `${index}`)];
+    appendElements(dom.bookDisplay, section);
+
+    //Flesh out the image section and add details section
+    const cardSection = [
+        createElement('div',`bookCard__image-container`, 0, divImg),
+        createElement('div',`bookCard__detail-container`)
+    ];
+    appendElements(section[0], cardSection);
+
+    //Flesh out the details section of card
+
+    const details = [
+        createElement('div', 'bookCard__detail-title', 0, divTitle),
+        createElement('div', 'bookCard__detail-subtitle', 0, divSubTitle),
+        createElement('div', 'bookCard__detail-categories', 0, divCategories),
+        createElement('div', 'bookCard__detail-description', 0, divDescription),
+        createElement('div', 'bookCard__detail-buttons', 0, divButtons),
+    ]
+    appendElements(cardSection[1], details);
+}
+
+
+
+
